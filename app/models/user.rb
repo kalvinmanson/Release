3fd323a15d4da6fff9d48316452bf5f87972fc36
Kalvin Manson <kalvinmanson@gmail.com>
2016-11-16 23:30:59 -0500

    Email registrant

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   belongs_to 	:city
   has_many		:comments
   has_many		:orders
--- db/schema.rb
+++ db/schema.rb
@@ -301,6 +301,10 @@ ActiveRecord::Schema.define(version: 20161114202844) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.string   "provider"
     t.string   "uid"
     t.index ["city_id"], name: "index_users_on_city_id", using: :btree
